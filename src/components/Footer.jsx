175b5c4b69a78d029cf291997682afd016606262
Cretu Claudia <cretuclaudia373@yahoo.com>
2024-12-19 11:44:13 +0100

    adding resume replacement and reading guide in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import ReadingGuideFour from "../assets/pdf/Personal/ReadingGuide4.0.pdf";
+import resume from "../assets/pdf/Personal/resume.pdf";
+
 const Footer = () => {
   return (
     <footer className="bg-darkPurple text-whiteNormal font-body p-6  bottom-0 left-0 w-full">
@@ -13,8 +16,12 @@ const Footer = () => {
         <div>
           <h2 className="text-lg font-bold font-header mb-2">Useful Links</h2>
           <ul className="space-y-2 font-body">
-            <li><a href="/path-to-your-file1.pdf" className="hover:underline" target="_blank" rel="noopener noreferrer">Resume</a></li>
-            <li><a href="/path-to-your-file2.pdf" className="hover:underline" target="_blank" rel="noopener noreferrer">Portfolio Reading Guide</a></li>
+            <li><a  href={resume} target="_blank" rel="noopener noreferrer" className="text-darkBlack hover:underline">
+                            Resume
+                        </a></li>
+            <li><a  href={ReadingGuideFour} target="_blank" rel="noopener noreferrer" className="text-darkBlack hover:underline">
+                            ReadingGuide
+                        </a></li>
           </ul>
         </div>
 
